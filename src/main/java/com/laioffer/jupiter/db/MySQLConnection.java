335a57e8cfe04cb45b861ef115abbdaa2cbdf170
doris2018 <36977334+doris2018@users.noreply.github.com>
2021-04-16 07:30:32 -0700

    Update MySQLConnection.java

--- src/main/java/com/laioffer/jupiter/db/MySQLConnection.java
+++ src/main/java/com/laioffer/jupiter/db/MySQLConnection.java
@@ -24,13 +24,14 @@ public class MySQLConnection {
     public MySQLConnection() throws MySQLException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            //是反射机制，程序运行到这行时，才确定java程序的behavior是什么
             conn = DriverManager.getConnection(MySQLDBUtil.getMySQLAddress());
         } catch (Exception e) {
             e.printStackTrace();
             throw new MySQLException("Failed to connect to Database");
         }
     }
-
+  //断开数据库的连接，释放资源，让程序运行效率高
     public void close() {
         if (conn != null) {
             try {
@@ -46,8 +47,15 @@ public class MySQLConnection {
             System.err.println("DB connection failed");
             throw new MySQLException("Failed to connect to Database");
         }
+        
+        //因为item实际上在twitch上，我们不可能把所有的item都insert到我们自己的Item table里，所以我们选择favorite一个，就insert一个到
+        //Item这个table里。
         saveItem(item);
+        
+        //这种用 ?,? 以及后面的PreparedStatement这个类，调用它的statement.setString()， 是为了更安全，所以习惯这种方式插入数据
         String sql = "INSERT IGNORE INTO favorite_records (user_id, item_id) VALUES (?, ?)";
+        //这个IGNORE目的是：当重复插入时（出现duplicate时），不会抛出异常，直接ignore（不会插入成功）。
+        
         try {
             PreparedStatement statement = conn.prepareStatement(sql);
             statement.setString(1, userId);
@@ -67,7 +75,7 @@ public class MySQLConnection {
         String sql = "DELETE FROM favorite_records WHERE user_id = ? AND item_id = ?";
         try {
             PreparedStatement statement = conn.prepareStatement(sql);
-            statement.setString(1, userId);
+            statement.setString(1, userId); //注意！！是从1开始，比较奇葩！
             statement.setString(2, itemId);
             statement.executeUpdate();
         } catch (SQLException e) {
@@ -97,7 +105,8 @@ public class MySQLConnection {
             throw new MySQLException("Failed to add item to Database");
         }
     }
-
+    
+    //为recommendation所用，调用这个方法，判断用户是否喜欢过，如果在favorite里，那么就不recommend给他。
     public Set<String> getFavoriteItemIds(String userId) throws MySQLException {
         if (conn == null) {
             System.err.println("DB connection failed");
@@ -110,9 +119,12 @@ public class MySQLConnection {
             PreparedStatement statement = conn.prepareStatement(sql);
             statement.setString(1, userId);
             ResultSet rs = statement.executeQuery();
+             //返回结果ResultSet是一个表格，每一行就是sql语句中 SELECT的数据，这里是item_id，只有这一列。
+            //这里的rs默认指向的是index = -1的位置，用iterator的.next()操作一个一个读出来。
+            
             while (rs.next()) {
-                String itemId = rs.getString("item_id");
-                favoriteItems.add(itemId);
+                String itemId = rs.getString("item_id");//注意，如果前面选中了100列，那么这里得写100行代码(用Hibernate简化)
+                favoriteItems.add(itemId);//往HashSet里加数据，注意，如果前面选中了100列，那么这里得写100行代码(用Hibernate简化)
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -122,6 +134,7 @@ public class MySQLConnection {
         return favoriteItems;
     }
 
+    //注意返回的是Map,key可以是 video, clip, stream; 对应的value是List
     public Map<String, List<Item>> getFavoriteItems(String userId) throws MySQLException {
         if (conn == null) {
             System.err.println("DB connection failed");
@@ -131,19 +144,23 @@ public class MySQLConnection {
         for (ItemType type : ItemType.values()) {
             itemMap.put(type.toString(), new ArrayList<>());
         }
+        //分了两步走，第一步先通过getFavoriteItemIds()，从favorite_records这个table里获得喜欢的游戏列表；
         Set<String> favoriteItemIds = getFavoriteItemIds(userId);
-        String sql = "SELECT * FROM items WHERE id = ?";
+      //第二步：从items这个table里获得喜欢items的所有(*)的信息
+        String sql = "SELECT * FROM items WHERE id = ?";//select了items的所有信息，id, url, type.....
+        
         try {
             PreparedStatement statement = conn.prepareStatement(sql);
             for (String itemId : favoriteItemIds) {
                 statement.setString(1, itemId);
                 ResultSet rs = statement.executeQuery();
                 if (rs.next()) {
-                    ItemType itemType = ItemType.valueOf(rs.getString("type"));
+                    ItemType itemType = ItemType.valueOf(rs.getString("type"));//String -> enum（ItemType）；enum是一个java class
+                    //！！！用了Item的.Builder().build()的方法，来构造item
                     Item item = new Item.Builder().id(rs.getString("id")).title(rs.getString("title"))
                             .url(rs.getString("url")).thumbnailUrl(rs.getString("thumbnail_url"))
                             .broadcasterName(rs.getString("broadcaster_name")).gameId(rs.getString("game_id")).type(itemType).build();
-                    itemMap.get(rs.getString("type")).add(item);
+                    itemMap.get(rs.getString("type")).add(item);//key为3种type，所以只有三个，value各对应一个List.
                 }
             }
         } catch (SQLException e) {
@@ -153,6 +170,7 @@ public class MySQLConnection {
         return itemMap;
     }
 
+    //作用跟getFavoriteItemIds() 相似，用于recommendation. 注意，得到的是一个Map，key是video，clip，stream三种类型
     public Map<String, List<String>> getFavoriteGameIds(Set<String> favoriteItemIds) throws MySQLException {
         if (conn == null) {
             System.err.println("DB connection failed");
@@ -216,6 +234,7 @@ public class MySQLConnection {
             statement.setString(4, user.getLastname());
 
             return statement.executeUpdate() == 1;
+     //如果成功插入了一行，executeUpdate()则返回1，没插入成功则返回0
         } catch (SQLException e) {
             e.printStackTrace();
             throw new MySQLException("Failed to get user information from Database");
