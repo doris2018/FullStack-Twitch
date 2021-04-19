da77c0b8cb6039f991192b4be20738899d306773
doris2018 <36977334+doris2018@users.noreply.github.com>
2021-04-19 07:56:26 -0700

    Update MySQLDBUtil.java

--- src/main/java/com/laioffer/jupiter/db/MySQLDBUtil.java
+++ src/main/java/com/laioffer/jupiter/db/MySQLDBUtil.java
@@ -4,13 +4,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-
+//这个class目的是生成要连接的DB的地址（DB在AWS上）
 public class MySQLDBUtil {
     private static final String INSTANCE = "javaproject-instance.c0ct3wf4pgon.us-east-2.rds.amazonaws.com";
     private static final String PORT_NUM = "3306";
     private static final String DB_NAME = "jupiter";
 
-
+   //是public static的方法，如果要连接数据库，可以直接调用它，返回的就是要连接的数据库的地址
     public static String getMySQLAddress() throws IOException {
         Properties prop = new Properties();
         String propFileName = "config.properties";
@@ -22,6 +22,8 @@ public class MySQLDBUtil {
 
         String username = prop.getProperty("user");
         String password = prop.getProperty("password");
+        
+       //jdbc意识是 java DB connector，这么写是一个格式
         return String.format("jdbc:mysql://%s:%s/%s?user=%s&password=%s&autoReconnect=true&serverTimezone=UTC&createDatabaseIfNotExist=true",
                 INSTANCE, PORT_NUM, DB_NAME, username, password);
     }
