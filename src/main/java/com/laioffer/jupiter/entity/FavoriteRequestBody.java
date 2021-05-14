0c31bd37bce48a4fa4220eef9f7d6d7df1491a15
doris2018 <36977334+doris2018@users.noreply.github.com>
2021-05-13 18:13:19 -0700

    Update FavoriteRequestBody.java

--- src/main/java/com/laioffer/jupiter/entity/FavoriteRequestBody.java
+++ src/main/java/com/laioffer/jupiter/entity/FavoriteRequestBody.java
@@ -3,6 +3,7 @@ package com.laioffer.jupiter.entity;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
 
+//Let’s first create the FavoriteRequestBody class which is used for deserializing add/remove favorite requests.
 public class FavoriteRequestBody {
     private final Item favoriteItem;
 
