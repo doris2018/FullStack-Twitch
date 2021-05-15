9452c2fd839982f21bd8561a5cbf221c4c42a5d2
doris2018 <36977334+doris2018@users.noreply.github.com>
2021-05-14 23:43:19 -0700

    Update Game.java

--- src/main/java/com/laioffer/jupiter/entity/Game.java
+++ src/main/java/com/laioffer/jupiter/entity/Game.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
+//if the data is null, do not convert
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonDeserialize(builder = Game.Builder.class)
