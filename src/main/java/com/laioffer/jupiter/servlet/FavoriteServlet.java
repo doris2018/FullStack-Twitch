a8a6b62418ce7618da5d479cdb3e854f6541055a
doris2018 <36977334+doris2018@users.noreply.github.com>
2021-05-18 18:39:17 -0700

    Update FavoriteServlet.java

--- src/main/java/com/laioffer/jupiter/servlet/FavoriteServlet.java
+++ src/main/java/com/laioffer/jupiter/servlet/FavoriteServlet.java
@@ -20,6 +20,7 @@ import java.util.Map;
 public class FavoriteServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession(false);
+        //if session == null, then there is no login
         if (session == null) {
             response.setStatus(HttpServletResponse.SC_FORBIDDEN);
             return;
