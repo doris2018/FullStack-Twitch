29aab34a75d9e6e814096dacc691d58dca748e46
doris2018 <36977334+doris2018@users.noreply.github.com>
2021-09-09 08:55:25 -0700

    Update Item.java

--- src/main/java/com/laioffer/jupiter/entity/Item.java
+++ src/main/java/com/laioffer/jupiter/entity/Item.java
@@ -1,5 +1,4 @@
 package com.laioffer.jupiter.entity;
-
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
